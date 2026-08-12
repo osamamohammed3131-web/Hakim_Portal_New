e72e2f1ca0e70aad75e9f4874619d631e3fe219e
osamamohammed3131-web <osamamohammed3131@gmail.com>
2026-08-12 23:27:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+from flask import Flask, redirect, url_for
+from extensions import db
+from models import User
+from admin import admin_bp
+from auth import auth_bp
+from dashboard import dashboard_bp
+from werkzeug.security import generate_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hakim_secure_secret_key_2026'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hakim.db'
+
+# ربط الإضافات
+db.init_app(app)
+
+# تسجيل الـ Blueprints
+app.register_blueprint(admin_bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(dashboard_bp)
+
+@app.route('/')
+def home():
+    return redirect(url_for('auth.login'))
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # إضافة حساب المشرف التلقائي
+        # إنشاء حساب المشرف تلقائياً بأمان تام
         admin = User.query.filter_by(email='superadmin@hakim.com').first()
         if not admin:
-            from werkzeug.security import generate_password_hash
             hashed_password = generate_password_hash('Admin@Hakim2026!', method='pbkdf2:sha256')
             new_admin = User(username='SuperAdmin', email='superadmin@hakim.com', password=hashed_password, is_admin=True)
             db.session.add(new_admin)
