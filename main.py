627bee94a53a26215660c722c0992d981309d3d6
osamamohammed3131-web <osamamohammed3131@gmail.com>
2026-08-13 18:49:43 +0300

    Configure database and update home route response
    Updated the home route response and added database configuration.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from extensions import db
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hakim.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
 @app.route("/")
 def home():
-    return "بوابة حكيم الجديدة تعمل بنجاح"
+    return "Hakim Academy"
 
 @app.route("/health")
 def health():
