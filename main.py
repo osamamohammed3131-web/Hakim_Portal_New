55bbab8530a313b88474c18cef262b74c45acfea
osamamohammed3131-web <osamamohammed3131@gmail.com>
2026-08-13 18:59:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+import os
+
 from flask import Flask
 from extensions import db
 
+
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hakim.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///hakim.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
+# تحميل نماذج قاعدة البيانات
+from models import User, StudyPlan, Subject, Lecture
+
+
 @app.route("/")
 def home():
     return "Hakim Academy"
+
 
 @app.route("/health")
 def health():
